315e45b11a09b7090816fb374b9a727e2ccb958e
PROBIEX <79619620+PROBIEX@users.noreply.github.com>
2021-12-31 10:06:05 +0530

    Update whatsapp_message.py

--- whatsapp_message.py
+++ whatsapp_message.py
@@ -1,2 +1,11 @@
 import pywhatkit as kit
-kit.sendwhatmsg("+919*********","probiex",09,49)
+import datetime
+now = datetime.datetime.now()
+a=int(input("after how many hours you want to send a message "))
+b=int(input("after how many minutes you want to send a message"))
+a = a + datetime.hours
+b = b + datetime.minutes
+try:
+    kit.sendwhatmsg("+919*********","your message content here",a,b)
+except:
+    print("an unexpected error occurred")
